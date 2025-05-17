3dae8d27bdc3f2ccb25a71643491638a382a8aba
Pavel-Klimenko <pavel.klimenko.1989@gmail.com>
2025-05-17 13:51:34 +0300

    Added chat functionality

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,9 @@
 <?php
+use App\Domains\Chat\Http\Controllers\ChatController;
+
+Route::group(['prefix' => 'chat', 'controller' => ChatController::class], function () {
+    Route::get('/', 'index');
+    Route::get('/messages', 'messages');
+    Route::post('/send', 'send');
+});
+
